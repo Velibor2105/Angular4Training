702438728f16d587c1bdaa6e091ca06349b80f8f
Velibor Stancic <v.stancic@vegaitsourcing.rs>
2018-05-31 16:01:33 +0200

    Added scripts

--- AccountClient/src/app/app.module.ts
+++ AccountClient/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule
