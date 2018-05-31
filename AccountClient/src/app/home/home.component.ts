83c11591144fa573943078cf7475dc37f07e6d1b
Velibor Stancic <v.stancic@vegaitsourcing.rs>
2018-05-31 15:33:47 +0200

    Home component modified

--- AccountClient/src/app/home/home.component.ts
+++ AccountClient/src/app/home/home.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public homeText: string;
   constructor() { }
 
   ngOnInit() {
+    this.homeText = "WELCOME TO ACCOUNT-OWNER APPLICATION";
   }
 
 }
